7c92e0bed57aba722d6c9e604464315adae5ad16
Raghad-2021 <Raghad.Eid.rae.js@tuwaiq.edu.sa>
2022-03-08 15:44:53 +0300

    test push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>Welcome  </p>
+       <p>Welcome 1 </p>
         
       </header>
     </div>
